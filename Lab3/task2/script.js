5f88aee7a17c2711f78f3bb5a88228635d995c37
Islam <ikabden04@gmail.com>
2026-02-09 12:29:03 +0500

    fdsf

--- Lab3/task2/script.js
+++ Lab3/task2/script.js
@@ -2,55 +2,10 @@ const todo__list = document.querySelector('.todo__list')
 const btn = document.querySelector('.todo__btn')
 const input = document.querySelector('.todo__in')
 
-class Todo{
-  #value = ""
-  #isDone = false
-  #id = 0
-
-  constructor(value, id , isDone = false) {
-    this.#id = id
-    this.#value = value
-    this.#isDone = isDone;
-  }
-
-  setValue(name){
-    this.#value = name
-  }
-
-  toggle(){
-    this.#isDone = !this.#isDone
-  }
-
-  getId(){
-    return this.#id
-  }
-
-  getValue(){
-    return this.#value
-  }
-
-  getIsDone(){
-    return this.#isDone
-  }
-
-  getInfo(){
-    return {
-      id: this.#id,
-      isDone: this.#isDone,
-      value: this.#value
-    }
-  }
-
-  static formJSON(obj){
-    return new Todo(obj.value, obj.id, obj.isDone, obj.id);
-  }
-
-}
 
 
 const getTasks = () => {
-  const raw = JSON.parse(localStorage.getItem('todoList')) || []
-  return raw.map(item => Todo.formJSON(item))
+  return JSON.parse(localStorage.getItem('todoList')) || [];
 }
 
 
@@ -67,19 +22,19 @@ const render = () => {
 const createTask = (todo) => {
 
    return `
-    <li class="todo__item ${todo.getIsDone() ? "done" : "" }">
-        <div onclick="toggleTask(${todo.getId()})" class="todo__check"></div>
-        <div class="todo__title">${todo.getValue()}</div>
-        <div class="todo__delete" onclick="removeTask(${todo.getId()})" ><img src="./img/delete_11042405.png" alt=""></div>
+    <li class="todo__item ${todo.isDone ? "done" : "" }">
+        <div onclick="toggleTask(${todo.id})" class="todo__check"></div>
+        <div class="todo__title">${todo.value}</div>
+        <div class="todo__delete" onclick="removeTask(${todo.id})" ><img src="./img/delete_11042405.png" alt=""></div>
     </li>
   `
 }
 
 const addTask = (e) => {
   let tasks = getTasks()
-  const todo = new Todo(input.value)
+  const todo = { id: tasks.length, value: input.value, isDone: false }
   tasks.push(todo)
-  localStorage.setItem('todoList', JSON.stringify(tasks.map(item => item.getInfo())))
+  localStorage.setItem('todoList', JSON.stringify(tasks))
   render()
 }
 
@@ -87,22 +42,20 @@ const toggleTask = (id) => {
   let tasks = getTasks()
 
   let newTasks = tasks.map((task, i) => {
-    if(task.getId() === id) {
-      task.toggle();
+    if(task.id === id) {
+      task.isDone = !task.isDone
     }
     return task
   })
-  localStorage.setItem('todoList', JSON.stringify(newTasks.map(item => item.getInfo())))
+  localStorage.setItem('todoList', JSON.stringify(newTasks))
   render()
 }
 
 const removeTask = (id) => {
   let todos = getTasks()
   if(todos){
-    // console.log(todos)
-    let newTodos = todos.filter(todo => todo.getId() !== id)
-    // console.log(newTodos)
-    localStorage.setItem('todoList', JSON.stringify(newTodos.map(item => item.getInfo())))
+    let newTodos = todos.filter(todo => todo.id !== id)
+    localStorage.setItem('todoList', JSON.stringify(newTodos))
   }
   render()
 }
@@ -110,3 +63,8 @@ const removeTask = (id) => {
 render()
 
 btn.addEventListener('click', addTask)
+input.addEventListener('keydown', (e) => {
+  if(e.key === "Enter") {
+    addTask()
+  }
+})
